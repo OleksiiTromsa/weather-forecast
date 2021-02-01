dc0aa57af4e6888a4e7900d725d557e65bce80d2
otromsa <otromsa@noltic.com>
2021-02-01 17:29:24 +0200

    Implement lwc weatherLWC component and controller for it. Little change in WeatherCallout

---
+++ force-app/main/default/lwc/weatherLWC/weatherLWC.js
@@ -0,0 +1,56 @@
+/**
+ * Created by oleksiitromsa on 29.01.2021.
+ */
+
+import { LightningElement, wire} from 'lwc';
+// import getWeather from '@salesforce/apex/WeatherController.getWeather';
+import getWeatherAndInsertToDB from
+    '@salesforce/apex/WeatherController.getWeatherAndInsertToDB';
+
+export default class WeatherLwc extends LightningElement {
+  error;
+  weather;
+  wind;
+  temperature;
+  pressure;
+  dt;
+
+  getWeatherForecast() {
+    this.city = this.template.querySelector("lightning-input").value;
+
+    getWeatherAndInsertToDB({city: this.city})
+      .then(data => {
+        this.weather = data;
+        this.wind = data.Wind_Speed__c;
+        this.temperature = Math.round(data.Temperature__c - 273.15);
+        this.pressure = data.Pressure__c;
+        this.dt = (new Date(Date.now())).toDateString();
+      })
+      .catch(error => {
+        this.error = error;
+      })
+  }
+
+  // @wire(getWeather)
+  // wiredWeather({data, error}) {
+  //   console.log('we got weather');
+  //   //console.log(this.data);
+  //   if (data) {
+  //     console.log('data not null');
+  //    // this.data = data;
+  //     console.log(data);
+  //     console.log(data.Wind_Speed__c);
+  //     this.wind = data.Wind_Speed__c;
+  //     let res = Object.assign([], data) ;
+  //     console.log(res.Wind_Speed__c);
+  //
+  //     //console.log(parsedData);
+  //
+  //     //console.log(wind);
+  //   }
+  //   else if (error) {
+  //     this.error = error;
+  //     console.log(error);
+  //   }
+  // }
+}
