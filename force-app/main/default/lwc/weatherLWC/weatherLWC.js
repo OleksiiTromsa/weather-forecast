eb11086e3cdff1c32961a8211d07747aea774b82
otromsa <otromsa@noltic.com>
2021-02-05 11:12:44 +0200

    Fixes according to comments

--- force-app/main/default/lwc/weatherLWC/weatherLWC.js
+++ force-app/main/default/lwc/weatherLWC/weatherLWC.js
@@ -2,9 +2,8 @@
  * Created by oleksiitromsa on 29.01.2021.
  */
 
-import { LightningElement, wire } from "lwc";
-// import getWeather from '@salesforce/apex/WeatherController.getWeather';
-import getWeatherAndInsertToDB from "@salesforce/apex/WeatherController.getWeatherAndInsertToDB";
+import { LightningElement } from "lwc";
+import createWeather from "@salesforce/apex/WeatherController.createWeather";
 
 export default class WeatherLwc extends LightningElement {
   error;
@@ -18,12 +17,12 @@ export default class WeatherLwc extends LightningElement {
   getWeatherForecast() {
     let inputCity = this.template.querySelector("lightning-input");
 
-    getWeatherAndInsertToDB({ city: inputCity.value })
+    createWeather({ city: inputCity.value })
       .then((data) => {
         this.city = inputCity.value;
         this.weather = data;
         this.wind = data.Wind_Speed__c;
-        this.temperature = Math.round(data.Temperature__c - 273.15);
+        this.temperature = Math.round(data.Temperature__c);
         this.pressure = data.Pressure__c;
         this.dt = new Date(Date.now()).toDateString();
       })
@@ -31,27 +30,4 @@ export default class WeatherLwc extends LightningElement {
         alert("Wrong city");
       });
   }
-
-  // @wire(getWeather)
-  // wiredWeather({data, error}) {
-  //   console.log('we got weather');
-  //   //console.log(this.data);
-  //   if (data) {
-  //     console.log('data not null');
-  //    // this.data = data;
-  //     console.log(data);
-  //     console.log(data.Wind_Speed__c);
-  //     this.wind = data.Wind_Speed__c;
-  //     let res = Object.assign([], data) ;
-  //     console.log(res.Wind_Speed__c);
-  //
-  //     //console.log(parsedData);
-  //
-  //     //console.log(wind);
-  //   }
-  //   else if (error) {
-  //     this.error = error;
-  //     console.log(error);
-  //   }
-  // }
 }
