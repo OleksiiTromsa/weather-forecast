2800287c103cc207b8dbdc81576ad1af36b1afe9
otromsa <otromsa@noltic.com>
2021-02-09 18:41:36 +0200

    Remove commented code

--- force-app/main/default/lwc/weatherLWC/weatherLWC.js
+++ force-app/main/default/lwc/weatherLWC/weatherLWC.js
@@ -5,20 +5,18 @@
 import { LightningElement, api } from "lwc";
 import createWeather from "@salesforce/apex/WeatherController.createWeather";
 
-
 export default class WeatherLwc extends LightningElement {
   @api city;
   weatherData;
 
   connectedCallback() {
-    createWeather({city : this.city})
+    createWeather({ city: this.city })
       .then((data) => {
         this.weatherData = data;
         console.log(data);
       })
       .catch((error) => {
-        alert('Check city name');
-        // console.log(error);
-      })
+        alert("Check city name");
+      });
   }
 }
