150137fe2ab27a463ed4827631f44a9712140d85
otromsa <otromsa@noltic.com>
2021-02-10 19:07:18 +0200

    Implement refresh button and divide logic to create and get weather

--- force-app/main/default/lwc/weatherLWC/weatherLWC.js
+++ force-app/main/default/lwc/weatherLWC/weatherLWC.js
@@ -2,23 +2,38 @@
  * Created by oleksiitromsa on 29.01.2021.
  */
 
-import { LightningElement, api } from "lwc";
+import { LightningElement, wire, api } from "lwc";
 import createWeather from "@salesforce/apex/WeatherController.createWeather";
+import getWeather from "@salesforce/apex/WeatherController.getWeather";
 import staticWeatherImage from "@salesforce/resourceUrl/WeatherImage";
 
 export default class WeatherLwc extends LightningElement {
   @api city;
+
   weatherData;
+  lastUpdateDate;
   weatherImage = staticWeatherImage;
 
-  connectedCallback() {
+  @wire(getWeather, {city: '$city'})
+  getWeather(response) {
+    if (response.data) {
+      this.weatherData = response.data;
+      this.lastUpdateDate = this.getDate();
+    }
+  }
+
+  refreshWeather() {
     createWeather({ city: this.city })
       .then((data) => {
         this.weatherData = data;
-        console.log(data);
+        this.lastUpdateDate = this.getDate();
       })
       .catch((error) => {
         alert("Check city name");
       });
+  }
+
+  getDate() {
+    return this.weatherData.Datetime__c.substring(0, 10);
   }
 }
